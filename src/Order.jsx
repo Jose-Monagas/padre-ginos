8b2c103e3863065dc69673a77ad58fcc683be012
Jose Monagas <jose.monagas@wirecutter.com>
2026-02-02 21:00:06 -0500

    creates context component

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import Pizza from "./Pizza";
 import Cart from './Cart';
+import { CartContext } from "./contexts";
 
 const intl = new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -11,7 +12,7 @@ export default function Order() {
     const [pizzaTypes, setPizzaTypes] = useState([]) // the pizza list from the server
     const [pizzaType, setPizzaType] = useState("pepperoni"); // which one is chosen 
     const [pizzaSize, setPizzaSize] = useState("M");
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useContext(CartContext);
     const [loading, setLoading] = useState(true);
 
     async function checkout() {
